8b40fec6195b60eb41277e349ccfac3fc54680c9
Lane <lf237@nau.edu>
2015-05-20 01:37:43 -0700

    nav include

--- ASL/main/urls.py
+++ ASL/main/urls.py
@@ -6,7 +6,6 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'main.views.views.home', name='home'),
     url(r'^content/(?P<id>\d+)/$', 'main.views.views.content_details'),
-    url(r'^facil/$', 'main.views.views.facil_list'),
     url(r'^(?P<difficulty>\w+)/$', 'main.views.views.difficulty'),
     url(r'^(?P<difficulty>\w+)/(?P<theme>\w+)/$', 'main.views.views.theme'),
 )
